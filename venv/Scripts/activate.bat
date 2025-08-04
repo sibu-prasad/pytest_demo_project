e65c8cffb6a6f62d90c7019d21ac884b9f48cdea
sibu-prasad <sibuprasadnanda@outlook.com>
2025-08-04 16:49:33 +0530

    New venv created and using requirement.txt file packages also installed.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sibup\OneDrive\Desktop\api_testing\pytest_project\venv
+set VIRTUAL_ENV=C:\Users\sibup\OneDrive\Desktop\api_testing\pytest_demo_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
